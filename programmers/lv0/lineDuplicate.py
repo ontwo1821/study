48dc5fbe11090e876671ce54516ac8d5589de50c
ontwo1821 <ontwo1821@gmail.com>
2023-04-19 13:20:41 +0900

    ong al e algorithm

---
+++ programmers/lv0/lineDuplicate.py
@@ -0,0 +1,14 @@
+def solution(lines):
+    answer = 0
+
+
+
+    return answer
+
+if __name__ == '__main__':
+
+    lines = [[0, 2], [-3, -1], [-2, 1]]
+    assert 2 == solution(lines)
+
+    # lines = [[0, 1], [2, 5], [3, 9]]
+    # assert 2 == solution(lines)
