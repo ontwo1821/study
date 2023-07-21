d12a47c5e1bae8dd211cb2451b63a48a93a86308
ontwo1821 <ontwo1821@gmail.com>
2023-07-22 02:01:45 +0900

    file remove

--- algorithm/programmers/lv2/out/production/lv2/missileDefense.py
+++
@@ -1,8 +0,0 @@
-def solution(targets):
-    answer = 0
-    return answer
-
-
-if __name__ == '__main__':
-    targets = [[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]
-    assert 3 == solution(targets)
